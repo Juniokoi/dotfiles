8b4f26b1d87b0c3316e8302a3e933fedb6d58c55
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-10-08 17:18:04 -0300

    refactor: Rearenge comments

--- NeoVim/.config/nvim/init.lua
+++ NeoVim/.config/nvim/init.lua
@@ -1,26 +1,26 @@
 -- Plugin Manager
-require("juniokoi.packer")    -- Main plugin manager
+require("juniokoi.packer")          -- Main plugin manager
 
 -- Options
-require("juniokoi.set")       -- NeoVim Settings
-require("juniokoi.keymaps")   -- Shortcuts
+require("juniokoi.set")             -- NeoVim Settings
+require("juniokoi.keymaps")         -- Shortcuts
 
 -- Tools
-require("juniokoi.cmp")       -- Autocompletion
-require("juniokoi.lsp")       -- Lisp Server
+require("juniokoi.cmp")             -- Autocompletion
+require("juniokoi.lsp")             -- Lisp Server
 
 -- Helpers
-require("juniokoi.telescope") -- Find everything within a second
-require("juniokoi.toggleterm")-- Generates a terminal as a popup
-require("juniokoi.autopairs") -- Make easier to code
+require("juniokoi.telescope")       -- Find everything within a second
+require("juniokoi.toggleterm")      -- Generates a terminal as a popup
+require("juniokoi.autopairs")       -- Make easier to code
 
 -- Essential
-require("juniokoi.comment")   -- Enable commenting code way easier
-require("juniokoi.treesitter")-- Prettify the code, giving colors
-require("juniokoi.nvim-tree") -- Because the stardand one sucks
+require("juniokoi.comment")         -- Enable commenting code way easier
+require("juniokoi.treesitter")      -- Prettify the code, giving colors
+require("juniokoi.nvim-tree")       -- Because the stardand one sucks
 
 -- Theme related
-require("juniokoi.lualine")   -- Manages the underbar info
-require("juniokoi.bufferline")-- Manages tabs and buffers
-require("juniokoi.gitsigns")  -- Show the user what lines and files did change with Git
+require("juniokoi.lualine")         -- Manages the underbar info
+require("juniokoi.bufferline")      -- Manages tabs and buffers
+require("juniokoi.gitsigns")        -- Show the user what lines and files did change with Git
 
