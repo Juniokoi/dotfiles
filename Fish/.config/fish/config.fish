27ca77ef24d65b6a6e0955ae136306bbec0c047b
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-10-08 14:36:46 -0300

    Add: Lazygit alias to fish shell

--- Fish/.config/fish/config.fish
+++ Fish/.config/fish/config.fish
@@ -1,46 +1,47 @@
 set fish_greeting
 if status is-interactive
-set XDG_CONFIG_HOME "$HOME/.config"
-set CONFIG "$XDG_CONFIG_HOME"
-set WM_CONFIG "$CONFIG/i3/config"
-set DOT "$HOME/.dotfiles"
+    set XDG_CONFIG_HOME "$HOME/.config"
+    set CONFIG "$XDG_CONFIG_HOME"
+    set WM_CONFIG "$CONFIG/i3/config"
+    set DOT "$HOME/.dotfiles"
 
-  alias vim nvim
-  alias fc "vim $CONFIG/fish/config.fish"
-  alias bc "vim $CONFIG/bspwm/bspwmrc"
-  alias sc "vim $CONFIG/sxhkd/sxhkdrc"
-  alias wc "vim $CONFIG/wezterm/wezterm.lua"
-  alias vc "vim $CONFIG/nvim/init.lua"
-  alias pc "vim $CONFIG/picom.conf"
-  alias wmc "vim $WM_CONFIG"
-  alias cdot "cd $DOT"
-  alias cfg "cd $CONFIG"
-  alias supersudo "sudo --preserve-env=PATH env"
-  alias c clear
-  alias cl clear
-  alias f fish
-  alias sayonara "sudo shutdown -h now"
-  alias logoff "sudo pkill -KILL -u $USER"
+    alias vim nvim
+    alias fc "vim $CONFIG/fish/config.fish"
+    alias bc "vim $CONFIG/bspwm/bspwmrc"
+    alias sc "vim $CONFIG/sxhkd/sxhkdrc"
+    alias wc "vim $CONFIG/wezterm/wezterm.lua"
+    alias vc "vim $CONFIG/nvim/init.lua"
+    alias pc "vim $CONFIG/picom.conf"
+    alias wmc "vim $WM_CONFIG"
+    alias cdot "cd $DOT"
+    alias cfg "cd $CONFIG"
+    alias supersudo "sudo --preserve-env=PATH env"
+    alias c clear
+    alias cl clear
+    alias f fish
+    alias sayonara "sudo shutdown -h now"
+    alias logoff "sudo pkill -KILL -u $USER"
 
-  alias ls "exa --icons --color=always --color-scale --sort type "
-  alias la "ls -@lah"
-  alias lt "ls -T"
-  alias cat "bat --style full --theme Dracula"
+    alias ls "exa --icons --color=always --color-scale --sort type "
+    alias la "ls -@lah"
+    alias lt "ls -T"
+    alias cat "bat --style full --theme Dracula"
 
-  alias au "yay -Syyuu --noconfirm"
+    alias au "yay -Syyuu --noconfirm"
 
-  ## GIT
-  alias gs "git-st"
-  alias gc "git commit -m"
-  alias gl "git lg"
-  alias glo "git log --oneline"
-  alias ga "git add"
+    ## GIT
+    alias gs "git-st"
+    alias gc "git commit -m"
+    alias gl "git lg"
+    alias glo "git log --oneline"
+    alias ga "git add"
+    alias lg "lazygit"
 
-  set PATH "$PATH:$HOME/.config/rofi/scripts:$HOME/.bin"
-  starship init fish | source
-  set fish_tmux_autostart true
-  set fish_tmux_unicode true
-  set fish_tmux_default_session_name "JunioKOI"
+    set PATH "$PATH:$HOME/.config/rofi/scripts:$HOME/.bin"
+    starship init fish | source
+    set fish_tmux_autostart true
+    set fish_tmux_unicode true
+    set fish_tmux_default_session_name "JunioKOI"
 
 
 end
