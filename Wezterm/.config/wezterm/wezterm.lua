f0eb0c9fdf17ed000b072430bc7dbda947a9cdf9
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-10-08 14:41:27 -0300

    Change [Wezterm]: colorscheme to tokyonight

--- Wezterm/.config/wezterm/wezterm.lua
+++ Wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 local colors = require('lua/rose-pine').colors()
 local window_frame = require('lua/rose-pine').window_frame()
 local wezterm = require('wezterm')
+
 return {
-  font_size = 12.0,
+  font_size = 13.0,
   -- colors = colors,
   window_frame = {
       active_titlebar_bg = '#000000',
   }, -- needed only if using fancy tab bar
-  color_scheme = "Catppuccin Mocha",
+  color_scheme = "tokyonight",
   warn_about_missing_glyphs=false,
   hide_tab_bar_if_only_one_tab = true,
- -- window_background_opacity = 0.50,
-
+  window_background_opacity = 0.70,
 }
