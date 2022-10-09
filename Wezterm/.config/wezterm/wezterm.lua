ee260bce527b464ba9f47cf3501253b2dbd64079
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-10-09 12:36:07 -0300

    docs: Add wezterm support to mutiple fonts

--- Wezterm/.config/wezterm/wezterm.lua
+++ Wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,20 @@
-local colors = require('lua/rose-pine').colors()
-local window_frame = require('lua/rose-pine').window_frame()
 local wezterm = require('wezterm')
 
 return {
-  font_size = 13.0,
-  -- colors = colors,
-  window_frame = {
-      active_titlebar_bg = '#000000',
-  }, -- needed only if using fancy tab bar
-  color_scheme = "tokyonight",
-  warn_about_missing_glyphs=false,
-  hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = 0.70,
+    font_size = 14.0,
+    line_height = 1.1,
+    font = wezterm.font_with_fallback {
+  --      'CaskaydiaCove Nerd Font',
+        'Fira Code',
+        'JetBrains Mono',
+    },
+ --   harfbuzz_features = {"ss01" , "calt" },
+    -- colors = colors,
+    window_frame = {
+        active_titlebar_bg = '#000000',
+    }, -- needed only if using fancy tab bar
+    color_scheme = "tokyonight",
+    warn_about_missing_glyphs = false,
+    hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 0.80,
 }
