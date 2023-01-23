b7ed33b537f60af698b4145cc04ed5cb6ec87f78
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2023-01-23 14:26:49 -0300

    feat: autoload xrandr settings with awesome

--- Awesome/.config/awesome/autorun.sh
+++ Awesome/.config/awesome/autorun.sh
@@ -7,5 +7,6 @@ function run {
 }
 
 run /usr/bin/emacs --daemon
+run xrandr --output HDMI-A-0 --primary --mode 2560x1080 --pos 0x0 --output DisplayPort-0 --mode 1920x1080 --pos 0x1080
 run setxkbmap -model pc104 -layout us,br -variant qwerty -option grp:win_space_toggle ctrl:swapcaps
 run picom --config $HOME/.config/picom.conf
