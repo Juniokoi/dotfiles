a71d4f57e01c618c515153b1add0bba57d511f60
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2023-03-23 22:28:24 -0300

    Add some nix changes

--- configuration.nix
+++ configuration.nix
@@ -1,261 +1,322 @@
 { config, pkgs, ... }:
 
 {
-	imports = [
-		./hardware-configuration.nix
+  imports = [ ./hardware-configuration.nix ];
+
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  services.teamviewer.enable = true;
+
+  # Configure keymap in X11
+  environment.pathsToLink = [ "/libexec" ];
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME    = "\${HOME}/.local/bin";
+    XDG_DATA_HOME   = "\${HOME}/.local/share";
+	XCURSOR_SIZE = "16";
+
+	PATH = [
+		"\${HOME}/.bin"
 	];
+  };
 
-	time.timeZone = "America/Sao_Paulo";
+  # Enable CUPS to print documents.
+  services = {
+    printing.enable = true;
+    emacs = {
+      enable = true;
+      install = true;
+      package = pkgs.emacsUnstable;
+    };
+  };
 
-# Select internationalisation properties.
-	i18n.defaultLocale = "en_US.UTF-8";
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+      sha256 = "0yikij7r12kspfhp3rqr8lnfw49bzl9jsv021kyimffg518y8ncv";
+    }))
+  ];
 
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "pt_BR.UTF-8";
-		LC_IDENTIFICATION = "pt_BR.UTF-8";
-		LC_MEASUREMENT = "pt_BR.UTF-8";
-		LC_MONETARY = "pt_BR.UTF-8";
-		LC_NAME = "pt_BR.UTF-8";
-		LC_NUMERIC = "pt_BR.UTF-8";
-		LC_PAPER = "pt_BR.UTF-8";
-		LC_TELEPHONE = "pt_BR.UTF-8";
-		LC_TIME = "pt_BR.UTF-8";
-	};
+  # Enable sound with pipewire.
+  sound.enable = true;
+  security.rtkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   #jack.enable = true;
+  # };
 
-    # Bootloader.
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.efi.efiSysMountPoint = "/boot/efi";
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  #Bluetooth
+  hardware.pulseaudio = {
+     enable = true;
+     package = pkgs.pulseaudioFull;
+   };
+  hardware.bluetooth = {
+    enable = true;
+    # settings = {
+    #   General = {
+    #     Enable = "Source,Sink,Media,Socket";
+    #   };
+    # };
+  };
+  services.blueman.enable = true;
+  # Forces a reset for specified bluetooth usb dongle.
+  systemd.services.fix-generic-usb-bluetooth-dongle = {
+    description = "Fixes for generic USB bluetooth dongle.";
+    wantedBy = [ "post-resume.target" ];
+    after = [ "post-resume.target" ];
+    script = builtins.readFile ./scripts/hack.usb.reset;
+    scriptArgs = "1131:1004"; # Vendor ID and Product ID here
+    serviceConfig.Type = "oneshot";
+  };
 
-	networking.hostName = "nixos"; # Define your hostname.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+  users.users.junio.shell = pkgs.fish;
+  environment.shells = with pkgs; [ fish ];
 
-    # Enable networking
-	networking.networkmanager.enable = true;
-	services.teamviewer.enable = true;
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    fira-code
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "FiraCode" ]; })
+  ];
 
+  programs.thunar = {
+    plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+    enable = true;
+  };
 
-# Configure keymap in X11
+  users.users.junio = {
+    isNormalUser = true;
 
-	environment.pathsToLink = [ "/libexec" ];
+    initialPassword = "123";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      bluez
+      bluez-tools
+      bluez-alsa
+      git
+      gcc
+      zlib
+      libtool
+      libffi
+      libyaml
+      patch
+      nodejs
+      openssl
 
-# Enable CUPS to print documents.
-	services = {
-		printing.enable = true;
-		emacs = {
-			enable = true;
-			install = true;
-			package = pkgs.emacsUnstable;
-		};
-	};
+      redshift
+      stylua
 
-	nixpkgs.overlays = [
-		(import (builtins.fetchTarball {
-			url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-			sha256 = "0yikij7r12kspfhp3rqr8lnfw49bzl9jsv021kyimffg518y8ncv";
-		}))
-	];
+      neofetch
+      statix
+      nixfmt
 
-    # Enable sound with pipewire.
-	sound.enable = true;
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-	};
+      obs-studio
+      xdg-desktop-portal
+      xdg-desktop-portal-wlr
+	  xdg-utils
+      vscode
+      ubuntu_font_family
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
+      jetbrains.idea-ultimate
+      solargraph
 
-	programs.fish.enable = true;
-	users.defaultUserShell = pkgs.fish;
-	users.users.junio.shell = pkgs.fish;
-	environment.shells = with pkgs; [ fish ];
+      vulkan-tools
+      vulkan-headers
+      amdvlk
+      driversi686Linux.amdvlk
 
-	fonts.fonts = with pkgs; [
-		noto-fonts
-		noto-fonts-cjk
-		noto-fonts-emoji
-		fira-code
-		(nerdfonts.override {fonts = [ "NerdFontsSymbolsOnly" "FiraCode" ]; })
-	];
+      jetbrains.ruby-mine
+      ruby_3_1
+      rubyPackages_3_1.rspec-core
+      rubyPackages_3_1.openssl
+      bundix
 
-	programs.thunar = {
-		plugins = with pkgs.xfce; [
-			thunar-archive-plugin
-			thunar-volman
-		];
-		enable = true;
-	};
+      notion-app-enhanced
+      libreoffice-qt
+      gimp
+      opentabletdriver
+      python3
+      starship
+      zip
+      unzip
+      fzf
+      obsidian
 
-	users.users.junio = {
-		isNormalUser = true;
-		description = "junio";
-		initialPassword = "123";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; [
-			git
-			zlib
-			libtool
-			libffi
-			libyaml
-			patch
-			openssl
-			joshuto
-			neofetch
-			statix
-			alejandra
-			gcc
-			zoxide
-			nodejs
+      authy
 
-			obs-studio
-			xdg-desktop-portal
-			xdg-desktop-portal-wlr
-			vscode
+      #Utilities for graphic card
+      xdotool
+      lshw
+      mesa
+      pciutils
+      toybox
 
-			jetbrains.idea-ultimate
-			jetbrains.ruby-mine
-			solargraph
+      pavucontrol
+      lua
+      luajit
+      sumneko-lua-language-server
+      sway-contrib.grimshot
+      waybar
+      gnumake
+      stow
+      cmake
+      gnupg
+      direnv
+      spotify
+      discord
+      htop
 
-			asdf-vm
+      # Oxidized tools
+      hwatch  # watch
+      joshuto # ranger
+      zoxide  # autoload, z
+      fd      # find
+      ripgrep # grep
+      gitui   # lazygit
+      bottom  # htop
+      zellij  # tmux
+      delta   # diff
+      du-dust # du
+    ];
+  };
 
-      # Ruby
-			ruby_3_1
-			rubyPackages_3_1.rspec-core
-			rubyPackages_3_1.openssl
-			bundix
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "";
+    digimend.enable = true; # Set Huion, XP-Pen, etc. tablets
+    desktopManager = {
+      xterm.enable = false;
+      gnome.enable = true;
+    };
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "junio";
+      };
+      sddm.enable = true;
+      defaultSession = "none+i3";
+    };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        i3status
+        i3lock
+        i3blocks
+        feh
+        rofi
+        dunst
+        xorg.xmodmap
+        xorg.xwininfo
+        xidlehook
+        xclip
+        flameshot
+        lxappearance
+      ];
+    };
+  };
+  # Enable automatic login for the user.
 
-			notion-app-enhanced
-			libreoffice-qt
-			gimp
-			opentabletdriver
-			python3
-			starship
-			zip
-			unzip
-			fzf
-			fd
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
-			authy
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-			ripgrep
-			pavucontrol
-			lua
-			luajit
-			sway-contrib.grimshot
-			waybar
-			gnumake
-			stow
-			cmake
-			gnupg
-			direnv
-			hwatch
-			spotify
-			discord
-			htop
-		];
-	};
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
 
-	services.xserver = {
-		enable = true;
-		layout = "us";
-		xkbVariant = "";
-		desktopManager = {
-			xterm.enable = false;
-			gnome.enable = true;
-		};
-		displayManager = {
-			autoLogin = {
-				enable = true;
-				user = "junio";
-			};
-			sddm.enable = true;
-			defaultSession = "none+i3";
-		};
-		windowManager.i3 = {
-			enable = true;
-			extraPackages = with pkgs; [
-				i3status
-				i3lock
-				i3blocks
-				feh
-				rofi
-				dunst
-				xorg.xmodmap
-				xorg.xwininfo
-				xidlehook
-				xclip
-				flameshot
-				lxappearance
-			];
-		};
-	};
-# Enable automatic login for the user.
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    firefox
+    (wrapOBS { plugins = with pkgs.obs-studio-plugins; [ wlrobs ]; })
+  ];
 
-    # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-	systemd.services."getty@tty1".enable = false;
-	systemd.services."autovt@tty1".enable = false;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-    # Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  networking = {
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "1.1.1.1" ];
+    # firewall = {
+    # allowedTCPPorts = [ ... ];
+    # allowedUDPPorts = [ ... ];
+    # enable = false;
+    # }
+  };
 
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
+  #nixpkgs.overlays = [
+  #(self: super: {
+  #discord = super.discord.overrideAttrs (
+  #_: { src = builtins.fetchTarball {
+  #url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+  #sha256 = "0000000000000000000000000000000000000000000000000";
+  #}; }
+  #);
+  #})
+  #];
 
-	environment.systemPackages = with pkgs; [
-		vim
-		wget
-		firefox
-	  (wrapOBS {
-			plugins = with pkgs.obs-studio-plugins; [
-			  wlrobs
-			];
-		})
-	];
+  system.stateVersion = "22.11"; # Change only if necessary
 
-	programs.gnupg.agent = {
-		enable = true;
-		enableSSHSupport = true;
-	};
+  ## OBS packages
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-	networking = {
-		defaultGateway = "192.168.0.1";
-		nameservers = [ "1.1.1.1" ];
-# firewall = {
-# allowedTCPPorts = [ ... ];
-# allowedUDPPorts = [ ... ];
-# enable = false;
-# }
-	};
-
-	#nixpkgs.overlays = [
-		#(self: super: {
-		 #discord = super.discord.overrideAttrs (
-				 #_: { src = builtins.fetchTarball {
-				 #url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-				 #sha256 = "0000000000000000000000000000000000000000000000000";
-				 #}; }
-				 #);
-		 #})
-	#];
-
-
-	system.stateVersion = "22.11"; # Change only if necessary
-
-	boot.extraModulePackages = with config.boot.kernelPackages; [
-		v4l2loopback
-	];
+  ## Graphic card
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-opencl-runtime
+  ];
+  environment.variables.VK_ICD_FILENAMES =
+    "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
 }
